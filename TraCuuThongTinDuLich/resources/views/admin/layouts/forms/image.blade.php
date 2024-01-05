bf007fd75cf3615f375ce86bd862b01e2e5a9a43
hyhloc <117262474+hyhloc@users.noreply.github.com>
2024-01-05 23:27:58 +0700

    Delete TraCuuThongTinDuLich directory

--- TraCuuThongTinDuLich/resources/views/admin/layouts/forms/image.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-	<label class="control-label col-md-2 col-sm-2 col-xs-12">@if($value['required']==1)<span class="form-asterick">* </span>@endif {!! $value['title'] !!}</label>
-	<div class="controls col-md-9 col-sm-10 col-xs-12">
-        <input type="file" name="{!! $value['name'] !!}" id="{!! $value['name'] !!}" multiple="multiple" accept="image/png, image/jpeg" class="cps-upload file_attrack file-browser" value="{!!$value['value']!!}" data-uploadimageadmin="{!! $value['name'] !!}">
-        <img src="{!!$value['value']!!}" class="thumb-img" style="width: 200px;">
-        @if($value['helper_text'] != '')<p class="help-block">{!! $value['helper_text'] !!}</p>@endif
-    </div>
-</div>
--- TraCuuThongTinDuLich/resources/views/errors/alert.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="col-lg-12">
-    @if(Session::has('flash_message'))
-        <div class="alert alert-{!! Session::get('flash_level') !!}">
-            {!! Session::get('flash_message') !!}
-            <p class="close-alert" style="display: inline;">X</p>
-        </div>
-    @endif
-
-</div>
