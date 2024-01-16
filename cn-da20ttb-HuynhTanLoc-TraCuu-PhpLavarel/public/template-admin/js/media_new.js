141fa70d9c200fb91248cbc88745b4f91956bfa8
hyhloc <117262474+hyhloc@users.noreply.github.com>
2024-01-16 21:36:53 +0700

    Delete cn-da20ttb-HuynhTanLoc-TraCuu-PhpLavarel directory

--- cn-da20ttb-HuynhTanLoc-TraCuu-PhpLavarel/public/template-admin/js/media_new.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', '.js-download-action', event => {
-    event.preventDefault();
-    $('#modal_download_url').modal('show');
-});
-
-$(document).on('click', '.js-create-folder-action', event => {
-    event.preventDefault();
-    $('#modal_add_folder').modal('show');
-});
